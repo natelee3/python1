501b3fda505a86c71dbdf3ac48b87e0ca60d4b62
Nate Lee <nleepercussion@gmail.com>
2021-05-05 19:49:55 -0400

    Simplified work or sleep in

--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -2,11 +2,9 @@
 #Prompt the user for a day of the week and print "Go to work" or "Sleep in" depending on if it's a workday
 
 day = int(input("Day (0-6)? "))
-if day == 0:
+if day == 0 or day == 6:
     print("Sleep in")
 elif 1 <= day <= 5 :
     print("Go to work")
-elif day == 6:
-    print("Sleep in")
 else:
     print("We'll try again later...")
