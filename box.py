316f93324fab27ba25b777a00c14b5afa49b7374
Nate Lee <nleepercussion@gmail.com>
2021-05-05 17:29:55 -0400

    Cleaned up box.py code

--- box.py
+++ box.py
@@ -1,15 +1,9 @@
 rows = int(input("Width? "))
 columns = int(input("Height? "))
 
-
-#for width_box in range(n):
- #   for height_box in range(2*n):
-  #      print('*' if width_box in(0,n-1) or height_box in(0,(2*n)-1) else ' ', end = ' ')
-   # print()
-
 for i in range(rows):
     for j in range(columns):
-        if (i == 0 or i == rows -1 or j == 0 or j == columns - 1):
+        if (i == 0 or i == rows - 1 or j == 0 or j == columns - 1):
             print('*', end = '  ')
         else: 
             print(' ', end = '  ')
